421278b100beda78b50e7363c949dd29bcdf4e62
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2012-04-16 12:53:41 +0000

    More cleanup

--- sorc/hiresw_post_hls.fd/OMGAOT_mod.f
+++
@@ -1,5 +0,0 @@
-      module omgaot
-      real, allocatable :: OMGA(:,:,:)
-      end module omgaot
-C
-C     COMMON /OMGAOT/ OMGA(IM,JM,LM)
--- sorc/hiresw_real_fcst.fd/dyn_nmm/init_modules_nmm.f90
+++
@@ -1,7 +0,0 @@
-
-
-SUBROUTINE init_modules_nmm
-
-
-END SUBROUTINE init_modules_nmm
-
