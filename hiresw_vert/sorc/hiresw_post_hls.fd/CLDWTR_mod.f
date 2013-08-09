f903d7ee45961e0936865bb8ff484126f39fae4b
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2013-08-09 19:39:35 +0000

    Deletes hiresw_post_hls.fd from repository

--- hiresw_vert/sorc/hiresw_post_hls.fd/CLDWTR_mod.f
+++
@@ -1,7 +0,0 @@
-      module cldwtr
-      real, allocatable ::
-     & CWM(:,:,:),U00(:,:),UL(:),LC(:,:),SR(:,:)
-      end module cldwtr
-C
-C                            C O M M O N /CLDWTR/ 
-C    & CWM(IM,JM,LM),U00(IM,JM),UL(2*LM),LC(IM,JM),SR(IM,JM)
