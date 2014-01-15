839b2bea5b0f8b1f37806d709a49ffc645865d08
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2014-01-15 13:21:30 +0000

    Some wps cleanup, and also unifies the 227 conus grid to use the same filename convention.

--- hiresw_vert/ush/hiresw_wps_ungrib_gen.sh
+++ hiresw_vert/ush/hiresw_wps_ungrib_gen.sh
@@ -18,8 +18,7 @@ cp $PARMhiresw/hiresw_Vtable.GFS Vtable
 if [ $MODEL != "nmmb" ]
 then
 cp ../namelist.wps.${stream} namelist.wps
-# cp $EXEChiresw/hiresw_ungrib_new ungrib.exe
-cp $EXEChiresw/hiresw_ungrib_3.5_src ungrib.exe
+cp $EXEChiresw/hiresw_ungrib ungrib.exe
 ./ungrib.exe >> $pgmout 2>errfile
 cp namelist.wps.${stream} ../run_ungrib/
 
