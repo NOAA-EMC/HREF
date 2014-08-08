3e8c47ecf3a68bd93ac120c2abd94cfbc237f829
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2014-08-08 18:28:48 +0000

    Various ush script changes.  Biggest to get the CONUS output grids working with grib2 and with approximately the right records.
    Also a metgrid change intended to help the preprap
    job work properly for CONUS.

--- hiresw.v6.1.0/ush/hiresw_prdgen_5km_grid_g2.sh
+++ hiresw.v6.1.0/ush/hiresw_prdgen_5km_grid_g2.sh
@@ -50,7 +50,7 @@ DOMOUT=${DOMIN_SMALL}${modelout}
 if [ $DOMIN = "conusnmmb" ]
 then
   filenamthree="wrf.CONUS05"
-  DOMIN_bucket="general"
+  DOMIN_bucket="general_g2"
   IM=1473
   JM=1025
 fi
@@ -58,7 +58,7 @@ fi
 if [ $DOMIN = "conusarw" ]
 then
   filenamthree="wrf.EMCONUS05"
-  DOMIN_bucket="general"
+  DOMIN_bucket="general_g2"
   IM=1473
   JM=1025
 fi
@@ -95,22 +95,75 @@ EOF5
 
 rm fort.*
 
-export pgm=hiresw_prdgen  ;. ./prep_step
+# export pgm=hiresw_prdgen  ;. ./prep_step
 
 
-if [ $DOMIN_SMALL = "conus" ]
-then
-export FORT21="$FIXhiresw/hiresw_wgt_${DOMIN}.g227"
-else
-export FORT21="$FIXhiresw/hiresw_wgt_${DOMIN}.g255_5km"
-fi
+# if [ $DOMIN_SMALL = "conus" ]
+# then
+# export FORT21="$FIXhiresw/hiresw_wgt_${DOMIN}.g227"
+# else
+# export FORT21="$FIXhiresw/hiresw_wgt_${DOMIN}.g255_5km"
+# fi
 
 export FORT10="master${fhr}.ctl"
 
 echo EXECUTING hiresw_prdgen  for 5 km
 
-export FORT621="input${fhr}.prd"
-$EXEChiresw/hiresw_prdgen  > prdgen.out${fhr}_5km 2>errfile_5km
+# export FORT621="input${fhr}.prd"
+# $EXEChiresw/hiresw_prdgen  > prdgen.out${fhr}_5km 2>errfile_5km
+
+### extract just needed items
+
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":PRES:mean sea level:"  -grib prmsl.grb
+
+echo collecting 1.grb
+time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(MSLET|VIS|GUST|VIL|MAXUVV|MAXDVV|REFD|REFC|MAXREF|MXUPHL|\
+TCOLI|TCOLR|TCOLS|TCOLC|TCOLW|LCDC|MCDC|HCDC|TCDC|RETOP|TSOIL|SOILW|PWAT|LFTX|4LFTX):" -grib 1.grb
+
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(TMAX|TMIN|MAXUW|MAXVW|MAXRH|MINRH):" -grib 2.grb
+
+echo collecting 3.grb
+time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(HINDEX|PRES|HGT|TMP|CSNOW|CICEP|CFRZR|CRAIN|LHTFL|SHTFL|CAPE|CIN):surface:" -grib 3.grb
+
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match "HGT:cloud base:" -grib cld.grb
+
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
+":(TMP|RH|UGRD|VGRD|PLI|POT|DPT|SPFH|MCONV|VVEL|CAPE|CIN):(30-0|60-30|90-60|120-90|150-120|180-0|90-0|255-0) mb above ground:" \
+-grib pbl.grb
+
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
+":(TMP|SPFH|DPT|RH|UGRD|VGRD|HLCY|USTM|VSTM|UPHL|PRES):(10|2|\
+1000-0|3000-0|6000-0|1524|80|5000-2000) m above (ground|mean sea level):" \
+-grib agl.grb
+
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'planetary boundary layer' -grib pbl2.grb
+
+
+echo collecting all_iso.grb
+time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
+":(UGRD|VGRD|TMP|HGT|VVEL|RH|DPT|ABSV):(200|250|300|350|400|450|500|525|550|575|\
+600|625|650|675|700|725|750|775|800|825|850|875|900|925|950|975|1000) mb:" \
+-grib all_iso.grb
+
+echo just apcp.grb
+time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'APCP' -grib apcp.grb
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'WEASD' -grib weasd.grb
+
+cat prmsl.grb apcp.grb weasd.grb 1.grb 2.grb 3.grb cld.grb pbl.grb pbl2.grb agl.grb all_iso.grb  > inputs.grb
+
+rm   prmsl.grb apcp.grb weasd.grb 1.grb 2.grb 3.grb cld.grb pbl.grb pbl2.grb  agl.grb all_iso.grb
+
+
+conus227="30 6 0 0 0 0 0 0 1473 1025 12190000 226541000 136 25000000 265000000 5079000 5079000 0 64 25000000 25000000"
+
+# copygb2 -g"${reg}" -x $INPUT_DATA/WRFPRS${fhr}.tm00 ${filenamthree}${fhr}.tm00
+
+echo copygb2 interp timing
+# time copygb2 -g"${conus227}" -x inputs.grb ${filenamthree}${fhr}.tm00
+
+# time $WGRIB2  inputs.grb  -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 iplib${fhr}.tm00
+# time /u/Wesley.Ebisuzaki/bin/wgrib2  inputs.grb  -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 iplib${fhr}.tm00
+time /u/Wesley.Ebisuzaki/bin/wgrib2  inputs.grb  -set_grib_type jpeg -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 ${filenamthree}${fhr}.tm00
 
 export err=$?;./err_chk
 
@@ -148,8 +201,8 @@ echo "inside f00 test"
 
   if test $SENDCOM = 'YES'
   then
-      cp ${filenamthree}${fhr}.tm00 $COMOUT/$DOMOUT.t${CYC}z.awp5kmf${fhr}
-      $utilexec/grbindex $COMIN/$DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmif${fhr}
+      cp ${filenamthree}${fhr}.tm00 $COMOUT/$DOMOUT.t${CYC}z.awp5kmf${fhr}.grib2
+#      $utilexec/grbindex $COMIN/$DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmif${fhr}
   fi
 else
 
@@ -213,7 +266,7 @@ done
 
   if test $SENDCOM = 'YES'
   then
-    cp $DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/.
-    $utilexec/grbindex $COMIN/$DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmif${fhr}
+     cp $DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmf${fhr}.grib2
+#    $utilexec/grbindex $COMIN/$DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmif${fhr}
   fi
 fi
