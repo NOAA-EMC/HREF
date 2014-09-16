01d2fe0dbb5e8722ca7e099724365520d5de135b
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2014-09-16 19:51:23 +0000

    Major tweaks to how fields are extracted from native file.  Also splits the big 5 km CONUS grid into four pieces.

--- hiresw.v6.1.0/ush/hiresw_prdgen_5km_grid_g2.sh
+++ hiresw.v6.1.0/ush/hiresw_prdgen_5km_grid_g2.sh
@@ -1,4 +1,4 @@
-#!/bin/ksh
+!/bin/ksh
 ################################################################################
 ####  UNIX Script Documentation Block
 #                      .                                             .
@@ -29,11 +29,12 @@ DOMIN_SMALL=$2
 CYC=${3}
 model=$4
 MEMBER=$5
+subpiece=$6
 
 reflag=1
 
-mkdir ${DATA}/prdgen_5km
-cd ${DATA}/prdgen_5km/
+mkdir ${DATA}/prdgen_5km_${subpiece}
+cd ${DATA}/prdgen_5km_${subpiece}/
 sh $utilscript/setup.sh
 
 DOMIN=${DOMIN_SMALL}${model}
@@ -80,9 +81,12 @@ export tmmark=tm00
 if [ $DOMIN_SMALL = "conus" ]
 then
 cp $PARMhiresw/hiresw_${model}_master.${DOMIN}.ctl_5km_g227 master${fhr}.ctl
+cp $PARMhiresw/hiresw_conus_awp5km.txt_${subpiece} hiresw_grid_extract.txt
 else
+cp $PARMhiresw/hiresw_conus_awp5km.txt hiresw_grid_extract.txt
 cp $PARMhiresw/hiresw_${model}_master.${DOMIN}.ctl_5km master${fhr}.ctl
 fi
+
 
 while [ ! -e $INPUT_DATA/postdone${fhr} ]
 do
@@ -114,68 +118,72 @@ echo EXECUTING hiresw_prdgen  for 5 km
 
 ### extract just needed items
 
-$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":PRES:mean sea level:"  -grib prmsl.grb
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":PRES:mean sea level:"  -grib prmsl.grb
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(MSLET|VIS|GUST|VIL|MAXUVV|MAXDVV|REFD|REFC|MAXREF|MXUPHL|\
+# TCOLI|TCOLR|TCOLS|TCOLC|TCOLW|LCDC|MCDC|HCDC|TCDC|RETOP|PWAT|LFTX|4LFTX):" -grib 1.grb
 
-echo collecting 1.grb
-time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(MSLET|VIS|GUST|VIL|MAXUVV|MAXDVV|REFD|REFC|MAXREF|MXUPHL|\
-TCOLI|TCOLR|TCOLS|TCOLC|TCOLW|LCDC|MCDC|HCDC|TCDC|RETOP|PWAT|LFTX|4LFTX):" -grib 1.grb
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(TMAX|TMIN|MAXUW|MAXVW|MAXRH|MINRH):" -grib 2.grb
 
-$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(TMAX|TMIN|MAXUW|MAXVW|MAXRH|MINRH):" -grib 2.grb
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(HINDEX|TSOIL|SOILW|CSNOW|CICEP|CFRZR|CRAIN):" -grib nn.grb
 
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(PRES|HGT|TMP|LHTFL|SHTFL|CAPE|CIN):surface:" -grib 3.grb
+
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match "HGT:cloud base:" -grib cld.grb
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match "HGT:cloud ceiling:" -grib ceiling.grb
+
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
+# ":(TMP|RH|UGRD|VGRD|PLI|POT|DPT|SPFH|MCONV|VVEL|CAPE|CIN):(30-0|60-30|90-60|120-90|150-120|180-0|90-0|255-0) mb above ground:" \
+# -grib pbl.grb
+
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
+# ":(TMP|SPFH|DPT|RH|UGRD|VGRD|HLCY|USTM|VSTM|UPHL|PRES):(10|2|\
+# 1000-0|3000-0|6000-0|1524|80|5000-2000) m above (ground|mean sea level):" \
+# -grib agl.grb
+
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'planetary boundary layer' -grib pbl2.grb
+
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
+# ":(UGRD|VGRD|TMP|HGT|VVEL|RH|DPT|ABSV):(200|250|300|350|400|450|500|525|550|575|\
+# 600|625|650|675|700|725|750|775|800|825|850|875|900|925|950|975|1000) mb:" \
+# -grib all_iso.grb
+
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'APCP' -grib apcp.grb
+# $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'WEASD' -grib weasd.grb
+
+# cat prmsl.grb  weasd.grb 1.grb 2.grb 3.grb cld.grb  pbl.grb pbl2.grb agl.grb all_iso.grb  > inputs.grb
+# cat nn.grb ceiling.grb  > inputs_nn.grb
+# cat apcp.grb > inputs_budget.grb
+
+# rm   prmsl.grb apcp.grb weasd.grb 1.grb 2.grb 3.grb cld.grb nn.grb ceiling.grb pbl.grb pbl2.grb  agl.grb all_iso.grb
+
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 | grep -F -f hiresw_grid_extract.txt | $WGRIB2 -i -grib inputs.grb $INPUT_DATA/WRFPRS${fhr}.tm00
+
+if [ $subpiece = "1" ]
+then
+$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(APCP|WEASD):" -grib inputs_budget.grb
 $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(HINDEX|TSOIL|SOILW|CSNOW|CICEP|CFRZR|CRAIN):" -grib nn.grb
-
-echo collecting 3.grb
-time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match ":(PRES|HGT|TMP|LHTFL|SHTFL|CAPE|CIN):surface:" -grib 3.grb
-
-$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match "HGT:cloud base:" -grib cld.grb
 $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match "HGT:cloud ceiling:" -grib ceiling.grb
-
-$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
-":(TMP|RH|UGRD|VGRD|PLI|POT|DPT|SPFH|MCONV|VVEL|CAPE|CIN):(30-0|60-30|90-60|120-90|150-120|180-0|90-0|255-0) mb above ground:" \
--grib pbl.grb
-
-$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
-":(TMP|SPFH|DPT|RH|UGRD|VGRD|HLCY|USTM|VSTM|UPHL|PRES):(10|2|\
-1000-0|3000-0|6000-0|1524|80|5000-2000) m above (ground|mean sea level):" \
--grib agl.grb
-
-$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'planetary boundary layer' -grib pbl2.grb
-
-
-echo collecting all_iso.grb
-time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match \
-":(UGRD|VGRD|TMP|HGT|VVEL|RH|DPT|ABSV):(200|250|300|350|400|450|500|525|550|575|\
-600|625|650|675|700|725|750|775|800|825|850|875|900|925|950|975|1000) mb:" \
--grib all_iso.grb
-
-echo just apcp.grb
-time $WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'APCP' -grib apcp.grb
-$WGRIB2 $INPUT_DATA/WRFPRS${fhr}.tm00 -match 'WEASD' -grib weasd.grb
-
-cat prmsl.grb  weasd.grb 1.grb 2.grb 3.grb cld.grb  pbl.grb pbl2.grb agl.grb all_iso.grb  > inputs.grb
-
-cat nn.grb ceiling.grb  > inputs_nn.grb
-
-cat apcp.grb > inputs_budget.grb
-
-rm   prmsl.grb apcp.grb weasd.grb 1.grb 2.grb 3.grb cld.grb nn.grb ceiling.grb pbl.grb pbl2.grb  agl.grb all_iso.grb
+cat nn.grb ceiling.grb > inputs_nn.grb
+/u/Wesley.Ebisuzaki/bin/wgrib2  inputs_nn.grb -new_grid_interpolation neighbor -set_grib_type complex2 -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 ${filenamthree}${fhr}.tm00_nn
+/u/Wesley.Ebisuzaki/bin/wgrib2  inputs_budget.grb -new_grid_interpolation budget -set_grib_type complex2 -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 ${filenamthree}${fhr}.tm00_budget
+fi
 
 
 conus227="30 6 0 0 0 0 0 0 1473 1025 12190000 226541000 136 25000000 265000000 5079000 5079000 0 64 25000000 25000000"
 
 # copygb2 -g"${reg}" -x $INPUT_DATA/WRFPRS${fhr}.tm00 ${filenamthree}${fhr}.tm00
-
-echo copygb2 interp timing
 # time copygb2 -g"${conus227}" -x inputs.grb ${filenamthree}${fhr}.tm00
-
 # time $WGRIB2  inputs.grb  -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 iplib${fhr}.tm00
 # time /u/Wesley.Ebisuzaki/bin/wgrib2  inputs.grb  -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 iplib${fhr}.tm00
 
-time /u/Wesley.Ebisuzaki/bin/wgrib2  inputs.grb  -set_grib_type complex2 -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 ${filenamthree}${fhr}.tm00_bilin
-time /u/Wesley.Ebisuzaki/bin/wgrib2  inputs_nn.grb -new_grid_interpolation neighbor -set_grib_type complex2 -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 ${filenamthree}${fhr}.tm00_nn
-time /u/Wesley.Ebisuzaki/bin/wgrib2  inputs_budget.grb -new_grid_interpolation neighbor -set_grib_type complex2 -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 ${filenamthree}${fhr}.tm00_budget
+/u/Wesley.Ebisuzaki/bin/wgrib2  inputs.grb  -set_grib_type complex2 -new_grid_winds grid -new_grid lambert:265:25:25 226.541:1473:5079 12.190:1025:5079 ${filenamthree}${fhr}.tm00_bilin
 
+if [ $subpiece = "1" ]
+then
 cat ${filenamthree}${fhr}.tm00_bilin ${filenamthree}${fhr}.tm00_nn ${filenamthree}${fhr}.tm00_budget > ${filenamthree}${fhr}.tm00
+else
+mv ${filenamthree}${fhr}.tm00_bilin ${filenamthree}${fhr}.tm00
+fi
 
 export err=$?;./err_chk
 
@@ -213,23 +221,28 @@ echo "inside f00 test"
 
   if test $SENDCOM = 'YES'
   then
-      cp ${filenamthree}${fhr}.tm00 $COMOUT/$DOMOUT.t${CYC}z.awp5kmf${fhr}.grib2
+      cp ${filenamthree}${fhr}.tm00 $COMOUT/$DOMOUT.t${CYC}z.awp5kmf${fhr}.grib2_${subpiece}
 #      $utilexec/grbindex $COMIN/$DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmif${fhr}
   fi
 else
 
+if [ $subpiece = "1" ]
+then
+echo COMPUTING PRECIP BUCKETS
+
 ### do precip buckets if model is ARW
 
-while [ ! -e $DATA/prdgen_5km/$filenamthree$onehrprev.tm00 ]
+# precip bucket subpieces can be changed to "1" if needed, as only will run for the first subpiece
+while [ ! -e $DATA/prdgen_5km_${subpiece}/$filenamthree$onehrprev.tm00 ]
 do
-echo waiting for $DATA/prdgen_5km/$filenamthree$onehrprev.tm00
+echo waiting for $DATA/prdgen_5km_${subpiece}/$filenamthree$onehrprev.tm00
 sleep 10
 done
 
 
   rm PCP1HR${fhr}.tm00
   rm input.card
-  echo "$DATA/prdgen_5km" > input.card
+  echo "$DATA/prdgen_5km_${subpiece}" > input.card
   echo $filenamthree >> input.card
   echo $onehrprev >> input.card
   echo $fhr >> input.card
@@ -247,16 +260,16 @@ done
 
 echo "3 hourly, do 3H precip bucket"
 
-while [ ! -e $DATA/prdgen_5km/$filenamthree$threehrprev.tm00 ]
+while [ ! -e $DATA/prdgen_5km_${subpiece}/$filenamthree$threehrprev.tm00 ]
 do
-echo waiting for $DATA/prdgen_5km/$filenamthree$threehrprev.tm00
+echo waiting for $DATA/prdgen_5km_${subpiece}/$filenamthree$threehrprev.tm00
 sleep 10
 done
 
 
   rm PCP3HR${fhr}.tm00
   rm input.card
-  echo "$DATA/prdgen_5km" > input.card
+  echo "$DATA/prdgen_5km_${subpiece}" > input.card
   echo $filenamthree >> input.card
   echo $threehrprev >> input.card
   echo $fhr >> input.card
@@ -274,11 +287,18 @@ done
    cat ${filenamthree}${fhr}.tm00  PCP1HR${fhr}.tm00  > $DOMOUT.t${CYC}z.awp5kmf${fhr}
   fi
 
+else
+
+mv ${filenamthree}${fhr}.tm00 $DOMOUT.t${CYC}z.awp5kmf${fhr}
+
+fi # subpiece=1
+
 ###### DONE PRECIP BUCKET
 
   if test $SENDCOM = 'YES'
   then
-     cp $DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmf${fhr}.grib2
+     cp $DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmf${fhr}.grib2_${subpiece}
 #    $utilexec/grbindex $COMIN/$DOMOUT.t${CYC}z.awp5kmf${fhr} $COMOUT/$DOMOUT.t${CYC}z.awp5kmif${fhr}
   fi
-fi
+
+fi # if f00 test
