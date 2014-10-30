ac7af92fccc89433ed2e14a954be1f466ae354e4
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2014-10-30 18:10:50 +0000

    Changes that seem to have the smartprecipg2 code producing proper fields for NMMB and ARW within HiresW system.

--- hiresw.v6.1.0/sorc/hiresw_smartprecipg2.fd/SMARTPRECIP.f90
+++ hiresw.v6.1.0/sorc/hiresw_smartprecipg2.fd/SMARTPRECIP.f90
@@ -1,4 +1,7 @@
        PROGRAM SMARTPRECP
+
+        USE GRIB_MOD
+
 !                .      .    .                                       .
 ! SUBPROGRAM:    SMARTPECIP
 !   PRGMMR: MANIKIN        ORG: W/NP22     DATE:  07-03-07
@@ -26,30 +29,24 @@
       CHARACTER*80 FNAME
       LOGICAL*1 LSUB
 
+!C grib2
+      INTEGER :: LUGB,LUGI,J,JDISC,JPDTN,JGDTN
+      INTEGER,DIMENSION(:) :: JIDS(200),JPDT(200),JGDT(200)
+      INTEGER,DIMENSION(:) :: PDS_SNOW_HOLD(200),PDS_RAIN_HOLD(200)
+      INTEGER,DIMENSION(:) :: PDS_SNOW_HOLD_EARLY(200), &
+                              PDS_RAIN_HOLD_EARLY(200)
+      LOGICAL :: UNPACK
+      INTEGER :: K,IRET,SUBINTVL,SUBSTART
+      TYPE(GRIBFIELD) :: GFLD
+!C grib2
+
+
       REAL,     ALLOCATABLE :: GRID(:)
       REAL,     ALLOCATABLE :: APCP1(:),APCP2(:),APCP3(:),APCP4(:)
       REAL,     ALLOCATABLE :: CAPCP1(:),CAPCP2(:),CAPCP3(:),CAPCP4(:)
       REAL,     ALLOCATABLE :: SNOW1(:),SNOW2(:),SNOW3(:),SNOW4(:)
       REAL,     ALLOCATABLE :: APCPOUT(:),CAPCPOUT(:),SNOWOUT(:)
       LOGICAL,  ALLOCATABLE :: MASK(:)
-!--------------------------------------------------------------------------
-      INTERFACE
-      SUBROUTINE RDHDRS(LUB,LUI,JPDS,JGDS,IGDN,IMAX,JMAX,KMAX,NUMV)
-      INTEGER,  INTENT(IN)     :: lub,lui                  ! unit numbers
-      INTEGER,  INTENT(INOUT)  :: jpds(200),jgds(200)      ! grib parms
-      INTEGER,  INTENT(OUT)    :: igdn,imax,jmax,kmax,numv ! grid size
-      END SUBROUTINE rdhdrs
-!--------------------------------------------------------------------------
-      SUBROUTINE SETVAR(LUB,LUI,NUMV,J,JPDS,JGDS,KF,K,KPDS,KGDS,MASK,GRID,VARB,IRET,ISTAT)
-      INTEGER,  INTENT(IN)     :: lub,lui,numv             ! unit numbers
-      INTEGER,  INTENT(INOUT)  :: jpds(200),jgds(200)      ! grib parms
-      INTEGER,  INTENT(OUT)    :: k,kf,kpds(200),kgds(200) ! grid info
-      INTEGER,  INTENT(OUT)    :: iret,istat               ! grid info
-      LOGICAL,  INTENT(INOUT)  :: MASK(:)                ! L/S mask 
-      REAL,     INTENT(INOUT)  :: GRID(:)                  ! grib data
-      REAL,     INTENT(OUT)    :: VARB(:)                  ! output varb
-      END SUBROUTINE setvar
-      END INTERFACE
 !--------------------------------------------------------------------------
 
       FNAME='fort.  '
@@ -63,7 +60,13 @@
 !==>  Make 3 hour buckets by subtracting fhr3 - fhr files
       LSUB=.FALSE.
 
+        writE(0,*) 'enter FHR1, FHR2, FHR3, FHR4: ', &
+                          FHR1, FHR2, FHR3, FHR4
+
       IF (FHR1.GT.FHR2) THEN
+        write(0,*) 'subtracting'
+        SUBINTVL=FHR1-FHR2
+        SUBSTART=FHR2
        FHR0=FHR2
        FHR2=FHR1
        FHR1=FHR0
@@ -78,6 +81,8 @@
        ENDIF
       ELSE
 
+        write(0,*) 'summing'
+
 !==>  sum up precip files
         if (fhr3 .lt. 0) FHR3=FHR1-3
 
@@ -86,9 +91,12 @@
                 FHR0=FHR1-3
         write(0,*) 'making 12 h precip'
         endif
+       write(0,*) 'summing fhr0,fhr1 fhr2 fhr3 fhr4 ',FHR0, FHR1, FHR2, FHR3,FHR4
       ENDIF
 
-!       print *, 'fhr0,fhr1 fhr2 fhr3 fhr4 ',FHR0, FHR1, FHR2, FHR3,FHR4
+        write(0,*) 'here with LSUB: ', LSUB
+
+!       write(0,*) 'summing fhr0,fhr1 fhr2 fhr3 fhr4 ',FHR0, FHR1, FHR2, FHR3,FHR4
 
       LUGB=13;LUGI=14; LUGB2=15;LUGI2=16
       LUGB3=17;LUGI3=18;LUGB4=19;LUGI4=20
@@ -96,13 +104,46 @@
 
       ISTAT = 0
 
-!=======================================================
-!  READ INDEX FILE TO GET GRID SPECS 
-!=======================================================
-      CALL RDHDRS(LUGB,LUGI,JPDS,JGDS,IGDNUM,IMAX,JMAX,KMAX,NUMVAL)
-
 ! -== GET SURFACE FIELDS ==-
 
+
+!        allocate(gfld%fld(1200*1200))
+        allocate(gfld%idsect(200))
+        allocate(gfld%igdtmpl(200))
+        allocate(gfld%ipdtmpl(200))
+        allocate(gfld%idrtmpl(200))
+!        allocate(gfld%bmap(1200*1200))
+
+        JIDS=-9999
+        JPDTN=-1
+        JPDT=-9999
+        JGDTN=-1
+        JGDT=-9999
+        UNPACK=.false.
+
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB
+        CALL BAOPENR(LUGB,FNAME,IRETGB)
+
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB2
+        CALL BAOPENR(LUGB2,FNAME,IRETGB)
+
+        write(0,*) 'trim(fname): ', trim(fname)
+
+        write(0,*) 'IRETGB on BAOPEN: ', IRETGB
+
+        call getgb2(LUGB2,LUGI2,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+
+        write(0,*) 'IRET from init getgb2 call: ', IRET
+
+        NUMVAL=gfld%ngrdpts
+
+        write(0,*) 'NUMVAL ', NUMVAL
+
+        if (IRET .ne. 0) STOP
+
+        UNPACK=.true.
+        
       ALLOCATE (MASK(NUMVAL),GRID(NUMVAL),STAT=kret)
       ALLOCATE (APCP1(NUMVAL),CAPCP1(NUMVAL),SNOW1(NUMVAL),STAT=kret)
       IF(kret.ne.0)THEN
@@ -111,19 +152,49 @@
       END IF
 
 !   PRECIP 
-      J = 0;JPDS = -1;JPDS(3) = IGDNUM
-      JPDS(5) = 061;JPDS(6) = 001
-      JPDS(13) = 1
-        write(0,*) 'read from LUGB: ', LUGB
-      CALL SETVAR(LUGB,LUGI,NUMVAL,J,JPDS,JGDS,KF,      &
-                K,KPDS,KGDS,MASK,GRID,APCP1,IRET,ISTAT)
+
+        write(0,*) 'have NUMVAL : ', NUMVAL
+
+        write(0,*) 'allocate again?'
+        allocate(gfld%fld(NUMVAL))
+        allocate(gfld%bmap(NUMVAL))
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+        UNPACK=.true.
+
+        call getgb2(LUGB,LUGI,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET_EARLY)
+
+        write(0,*) 'IRET from GETGB2: ', IRET_EARLY
+
+        if (IRET_EARLY .ne. 0) THEN
+        
+        write(0,*) 'set APCP1 to zero'
+        APCP1=0.
+
+        else
+        
+        write(0,*) 'size(APCP1): ', size(APCP1)
+        write(0,*) 'size(gfld%fld): ', size(gfld%fld)
+
+        APCP1=gfld%fld
+
+
+        do K=1,200
+        PDS_RAIN_HOLD_EARLY(K)=gfld%ipdtmpl(K)
+        enddo
+
+        endif
 
 !  CONVECTIVE PRECIP
 !      J = 0;JPDS = -1;JPDS(3) = IGDNUM
 !      JPDS(5) = 063;JPDS(6) = 001
 !      JPDS(13) = 1
-!      CALL SETVAR(LUGB,LUGI,NUMVAL,J,JPDS,JGDS,KF,       &
-!                 K,KPDS,KGDS,MASK,GRID,CAPCP1,IRET,ISTAT)
 !
 !  SNOWFALL 
       J = 0;JPDS = -1;JPDS(3) = IGDNUM
@@ -138,14 +209,37 @@
         write(0,*) 'JPDS(14) for snow now: ', JPDS(14)
 
         endif
-      CALL SETVAR(LUGB,LUGI,NUMVAL,J,JPDS,JGDS,KF,      &
-                 K,KPDS,KGDS,MASK,GRID,SNOW1,IRET,ISTAT)
+
+        JIDS=-9999
+        JPDTN=1
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+
+        if (IRET .ne. 0) THEN
+        
+        write(0,*) 'set SNOW1 to zero'
+        SNOW1=0.
+
+        else
+
+        SNOW1=gfld%fld
+
+        do K=1,200
+        PDS_SNOW_HOLD_EARLY(K)=gfld%ipdtmpl(K)
+        enddo
+        
+        endif
+
 
 !=======================================================
-!  READ INDEX FILE TO GET GRID SPECS for 2nd file
+!  READ 2nd file
 !=======================================================
-      CALL RDHDRS(LUGB2,LUGI2,JPDS,JGDS,IGDNUM,IMAX,JMAX,KMAX,NUMVAL)
-      JGDS=-1
 
       ALLOCATE (APCP2(NUMVAL),CAPCP2(NUMVAL),SNOW2(NUMVAL),STAT=kret)
       IF(kret.ne.0)THEN
@@ -154,19 +248,26 @@
       END IF
 
 !     ACCUMULATED PRECIP 
-      J = -1;JPDS = -1;JPDS(3) = IGDNUM
-      JPDS(5) = 061;JPDS(6) = 001
-      JPDS(13) = 1 
-        write(0,*) 'read from LUGB2: ', LUGB2
-      CALL SETVAR(LUGB2,LUGI2,NUMVAL,J,JPDS,JGDS,KF,     &
-                 K,KPDS,KGDS,MASK,GRID,APCP2,IRET,ISTAT)
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB2,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        APCP2=gfld%fld
+
+        do K=1,200
+        PDS_RAIN_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
 
 !     ACCUMULATED CONVECTIVE PRECIP
 !      J = 0;JPDS = -1;JPDS(3) = IGDNUM
 !      JPDS(5) = 063;JPDS(6) = 001
 !      JPDS(13) = 1
-!      CALL SETVAR(LUGB2,LUGI2,NUMVAL,J,JPDS,JGDS,KF,     &
-!                 K,KPDS,KGDS,MASK,GRID,CAPCP2,IRET,ISTAT)
 !
 !     SNOWFALL
       J = 0;JPDS = -1;JPDS(3) = IGDNUM
@@ -176,35 +277,63 @@
       JPDS(15) = FHR2
       IF (LSUB) JPDS(14)=FHR3
         endif
-      CALL SETVAR(LUGB2,LUGI2,NUMVAL,J,JPDS,JGDS,KF,     &
-                 K,KPDS,KGDS,MASK,GRID,SNOW2,IRET,ISTAT)
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB2,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+
+        SNOW2=gfld%fld
+
+        do K=1,200
+        PDS_SNOW_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
 
       IF (FHR4.GT.0 ) THEN
 
 !=======================================================
-!  READ INDEX FILE TO GET GRID SPECS for 3rd file
+!  READ 3rd file
 !=======================================================
-      CALL RDHDRS(LUGB3,LUGI3,JPDS,JGDS,              &
-                  IGDNUM,IMAX,JMAX,KMAX,NUMVAL)
+!      CALL RDHDRS(LUGB3,LUGI3,JPDS,JGDS,              &
+!                  IGDNUM,IMAX,JMAX,KMAX,NUMVAL)
+
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB3
+        CALL BAOPENR(LUGB3,FNAME,IRETGB)
 
       ALLOCATE (APCP3(NUMVAL),CAPCP3(NUMVAL),SNOW3(NUMVAL),STAT=kret)
       IF(kret.ne.0)THEN
        WRITE(*,*)'ERROR allocation source location: ',numval
        STOP
       END IF
+
 !     ACCUMULATED PRECIP 
-      J = 0;JPDS = -1;JPDS(3) = IGDNUM
-      JPDS(5) = 061;JPDS(6) = 001
-      JPDS(13) = 1
-      CALL SETVAR(LUGB3,LUGI3,NUMVAL,J,JPDS,JGDS,KF,      &
-                 K,KPDS,KGDS,MASK,GRID,APCP3,IRET,ISTAT)
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB3,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        APCP3=gfld%fld
+
+        do K=1,200
+        PDS_RAIN_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
 
 !     ACCUMULATED CONVECTIVE PRECIP
 !      J = 0;JPDS = -1;JPDS(3) = IGDNUM
 !      JPDS(5) = 063;JPDS(6) = 001
 !      JPDS(13) = 1
-!      CALL SETVAR(LUGB3,LUGI3,NUMVAL,J,JPDS,JGDS,KF,      &
-!                 K,KPDS,KGDS,MASK,GRID,CAPCP3,IRET,ISTAT)
 
 !     SNOWFALL
       J = 0 ;JPDS = -1;JPDS(3) = IGDNUM
@@ -213,14 +342,28 @@
         JPDS(14) = FHR2
         JPDS(15) = FHR3
         endif
-      CALL SETVAR(LUGB3,LUGI3,NUMVAL,J,JPDS,JGDS,KF,      &
-                 K,KPDS,KGDS,MASK,GRID,SNOW3,IRET,ISTAT)
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB3,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        SNOW3=gfld%fld
+
+        do K=1,200
+        PDS_SNOW_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
 
 !=======================================================
-!  READ INDEX FILE TO GET GRID SPECS for 4th file
+!  READ 4th file
 !=======================================================
-      CALL RDHDRS(LUGB4,LUGI4,JPDS,JGDS,                  &
-                  IGDNUM,IMAX,JMAX,KMAX,NUMVAL)
+
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB4
+        CALL BAOPENR(LUGB4,FNAME,IRETGB)
 
       ALLOCATE (APCP4(NUMVAL),CAPCP4(NUMVAL),SNOW4(NUMVAL),STAT=kret)
       IF(kret.ne.0)THEN
@@ -231,15 +374,41 @@
       J = 0;JPDS = -1;JPDS(3) = IGDNUM
       JPDS(5) = 061;JPDS(6) = 001
       JPDS(13) = 1
-      CALL SETVAR(LUGB4,LUGI4,NUMVAL,J,JPDS,JGDS,KF,     &
-                 K,KPDS,KGDS,MASK,GRID,APCP4,IRET,ISTAT)
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB4,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        APCP4=gfld%fld
+
+        do K=1,200
+        PDS_RAIN_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
 
 !     ACCUMULATED CONVECTIVE PRECIP
 !      J = 0;JPDS = -1;JPDS(3) = IGDNUM
 !      JPDS(5) = 063;JPDS(6) = 001
 !      JPDS(13) = 1
-!      CALL SETVAR(LUGB4,LUGI4,NUMVAL,J,JPDS,JGDS,KF,       &
-!                 K,KPDS,KGDS,MASK,GRID,CAPCP4,IRET,ISTAT)
+
+!     SNOWFALL
+      J = 0 ;JPDS = -1;JPDS(3) = IGDNUM
+      JPDS(5) = 065;JPDS(6) = 001
+        if (IARW .eq. 0) then
+        JPDS(14) = FHR2
+        JPDS(15) = FHR3
+        endif
+
+
+!     ACCUMULATED CONVECTIVE PRECIP
+!      J = 0;JPDS = -1;JPDS(3) = IGDNUM
+!      JPDS(5) = 063;JPDS(6) = 001
+!      JPDS(13) = 1
 !
 !     SNOWFALL
       J = 0 ;JPDS = -1;JPDS(3) = IGDNUM
@@ -248,8 +417,23 @@
         JPDS(14) = FHR3
         JPDS(15) = FHR4
         endif
-      CALL SETVAR(LUGB4,LUGI4,NUMVAL,J,JPDS,JGDS,KF,      &
-                 K,KPDS,KGDS,MASK,GRID,SNOW4,IRET,ISTAT)
+
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB4,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        SNOW4=gfld%fld
+
+        do K=1,200
+        PDS_SNOW_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
 
       ENDIF 
 
@@ -261,6 +445,11 @@
        WRITE(*,*)'ERROR allocation source location: ',numval
        STOP
       END IF
+
+
+!! LSUB --> 3 h total
+!! FHR4 > 0 --> 12 h total
+!! nothing --> 6 h total
 
       IF (LSUB) THEN
        APCPOUT=APCP2-APCP1
@@ -278,8 +467,65 @@
        ENDIF
       ENDIF
 
-      KPDS(14)=FHR3
-      KPDS(15)=FHR2
+
+! convert these to GRIB2 equivs
+
+!      KPDS(14)=FHR3
+!      KPDS(15)=FHR2
+        
+        if (IRET_EARLY .ne. 0) then
+          gfld%ipdtmpl=PDS_RAIN_HOLD
+        else
+          gfld%ipdtmpl=PDS_RAIN_HOLD_EARLY
+        endif
+
+        gfld%ipdtmpl(9)=ihrs1
+
+        do J=16,21
+        gfld%ipdtmpl(J)=PDS_RAIN_HOLD(J)
+        enddo
+
+        gfld%ipdtmpl(22)=1
+
+
+! default as a 6 h accumulation?
+        write(0,*) 'here FHR3, FHR2: ', FHR3, FHR2
+
+!        if (LSUB) then
+
+        gfld%ipdtmpl(27)=SUBINTVL
+        gfld%ipdtmpl(9)=SUBSTART
+
+
+!! use of (27) here looks wrong!
+
+!        write(0,*) 'gfld%ipdtmpl(27) bef: ', &
+!                    gfld%ipdtmpl(27)
+!        write(0,*) 'gfld%ipdtmpl(9) bef: ', &
+!                    gfld%ipdtmpl(9)
+
+!        gfld%ipdtmpl(27)=3
+!        gfld%ipdtmpl(9)=FHR1
+
+       write(0,*) 'gfld%ipdtmpl(27) aft: ', &
+                   gfld%ipdtmpl(27)
+       write(0,*) 'gfld%ipdtmpl(9) aft: ', &
+                   gfld%ipdtmpl(9)
+
+!        endif
+
+        if (FHR4 .GT.0) then
+        gfld%ipdtmpl(27)=12
+        gfld%ipdtmpl(9)=FHR0
+        endif
+
+        if (FHR4 .LT. 0 .and. .not.(LSUB)) then
+        gfld%ipdtmpl(27)=6
+        gfld%ipdtmpl(9)=FHR3
+        endif
+        
+        gfld%fld=APCPOUT
+
       IF (LSUB) KPDS(14)=FHR1
       IF (FHR4.GT.0)THEN
         KPDS(14)=FHR0
@@ -290,7 +536,8 @@
       print *, 'writing precip', KPDS(5),KPDS(14),KPDS(15),LUGB5,MAXVAL(APCPOUT)
       WRITE(FNAME(6:7),FMT='(I2)')LUGB5
       CALL BAOPEN(LUGB5,FNAME,IRETGB)
-      CALL PUTGB(LUGB5,NUMVAL,KPDS,KGDS,MASK,APCPOUT,IRET)
+!      CALL PUTGB(LUGB5,NUMVAL,KPDS,KGDS,MASK,APCPOUT,IRET)
+      call putgb2(LUGB5,GFLD,IRET)
       CALL BACLOSE(LUGB5,IRET)
 
 !      KPDS(5)=63
@@ -300,116 +547,16 @@
 !      CALL PUTGB(LUGB6,NUMVAL,KPDS,KGDS,MASK,CAPCPOUT,IRET)
 !      CALL BACLOSE(LUGB6,IRET)
 
+      gfld%ipdtmpl(2)=13
+      gfld%fld=SNOWOUT
+
       KPDS(5)=65
       print *, 'writing SNOW', KPDS(5),KPDS(14),KPDS(15),LUGB7, MAXVAL(SNOWOUT)
       WRITE(FNAME(6:7),FMT='(I2)')LUGB7
       CALL BAOPEN(LUGB7,FNAME,IRET)
-      CALL PUTGB(LUGB7,NUMVAL,KPDS,KGDS,MASK,SNOWOUT,IRET)
+!      CALL PUTGB(LUGB7,NUMVAL,KPDS,KGDS,MASK,SNOWOUT,IRET)
+      call putgb2(LUGB7,GFLD,IRET)
       CALL BACLOSE(LUGB7,IRET)
 
       STOP
-      END
-
-      SUBROUTINE SETVAR(LUB,LUI,NUMV,J,JPDS,JGDS,KF,K,KPDS,KGDS,MASK,GRID,VARB,IRET,ISTAT)
-!============================================================================
-!     This Routine reads in a grib field and initializes a 2-D variable
-!     Requested from w3lib GETGRB routine
-!     10-2012   Jeff McQueen
-!     NOTE: ONLY WORKS for REAL Type Variables
-!============================================================================
-      INTEGER,  INTENT(IN)     :: lub,lui,numv             ! unit numbers
-      INTEGER,  INTENT(INOUT)  :: jpds(200),jgds(200)      ! grib parms
-      INTEGER,  INTENT(OUT)    :: k,kf,kpds(200),kgds(200) ! grid info
-      INTEGER,  INTENT(OUT)    :: iret,istat               ! grid info
-      LOGICAL,  INTENT(INOUT) :: MASK(:)                  ! L/S mask 
-      REAL,     INTENT(INOUT)  :: GRID(:)                  ! grib data
-      REAL,     INTENT(OUT)    :: VARB(:)                  ! output varb
-
-!     Get GRIB Variable
-      CALL GETGB(LUB,LUI,NUMV,J,JPDS,JGDS,KF,K,KPDS,KGDS,MASK,GRID,IRET)
-      IF(IRET.EQ.0) THEN
-        DO KK = 1, NUMV
-          VARB(KK) = GRID(KK)
-        ENDDO
-        WRITE(6,100) JPDS(5),JPDS(6),JPDS(7),J,MAXVAL(VARB),KF,K
- 100    FORMAT('VARB UNPACKED ', 4I7,G12.4,I8, ' RECORD',I5)
-      ELSE
-        WRITE(0,*)'===================================================='
-        WRITE(0,*)'SETVAR : COULD NOT UNPACK VARB',JPDS(5),JPDS(6)
-        WRITE(0,*)' J ',J,' GRID ',JPDS(3),'GETGB RETURN CODE',IRET
-        WRITE(0,*)'UNIT', LUB,LUI,'NUMVAL ', NUMV,KF,' RECORD',K
-        WRITE(0,*)'===================================================='
-        WRITE(0,*) 'JPDS ', JPDS(1:25)
-!        print *, 'JGDS', JGDS(1:25)
-        ISTAT = IRET
-        ERR=IRET
-        STOP 'UNIFPRECIP ABORT:   VARB NOT UNPACKED'
-      ENDIF
-
-      RETURN
-      END
-
-      SUBROUTINE RDHDRS(LUB,LUI,JPDS,JGDS,IGDN,IMAX,JMAX,KMAX,NUMV)
-!=============================================================================
-!     This Routine Reads GRIB index file and returns its contents
-!     (GETGI)
-!     Also reads GRIB index and grib file headers to
-!     find a GRIB message and unpack pds/gds parameters (GETGB1S)
-!
-!     10-2012  Jeff McQueen
-!==============================================================================
-      INTEGER,    INTENT(IN)     :: lub,lui                  ! unit numbers
-      INTEGER,    INTENT(INOUT)  :: jpds(200),jgds(200)      ! grib parms
-      INTEGER,    INTENT(OUT)    :: igdn,imax,jmax,kmax,numv ! grid size
-
-      INTEGER,PARAMETER :: MBUF = 2000000 !Character length of bufr varb
-      CHARACTER*80 FNAME
-      CHARACTER CBUF(MBUF)
-      INTEGER KPDS(200),KGDS(200)
-      INTEGER JENS(200),KENS(200)
-
-!jtm  Input Filename prefix on WCOSS
-      FNAME='fort.  '
-
-      IRGI = 1
-      IRGS = 1
-      KMAX = 0
-      JR=0
-      KSKIP = 0
-
-      WRITE(FNAME(6:7),FMT='(I2)')LUB
-      CALL BAOPEN(LUB,FNAME,IRETGB)
-      WRITE(FNAME(6:7),FMT='(I2)')LUI
-      CALL BAOPEN(LUI,FNAME,IRETGI)
-
-      CALL GETGI(LUI,KSKIP,MBUF,CBUF,NLEN,NNUM,IRGI)
-
-      write(6,*)' IRET FROM GETGI ',IRGI,' UNIT ',LUB,LUI,' NLEN',NLEN
-      IF(IRGI .NE. 0) THEN
-        WRITE(6,*)' PROBLEMS READING GRIB INDEX FILE SO ABORT',IRGI
-        ISTAT = IRGI
-        STOP 'RDHDRS ABORT GETGI'
-      ENDIF
-
-       DO K = 1, NNUM
-        JR = K - 1
-        JPDS = -1
-        JGDS = -1
-        CALL GETGB1S(CBUF,NLEN,NNUM,JR,JPDS,JGDS,JENS,    &
-                     KR,KPDS,KGDS,KENS,LSKIP,LGRIB,IRGS)
-        IF(IRGI .NE. 0) THEN
-          WRITE(6,*)' PROBLEMS ON 1ST READ OF GRIB FILE SO ABORT',IRGS
-          ISTAT = IRGS
-          STOP 'RDHDRS ABORT: GETBS1S'
-        ENDIF
-        IGDN = KPDS(3)
-        IMAX = KGDS(2)
-        JMAX = KGDS(3)
-        NUMV = IMAX*JMAX
-        KMAX = 0    ! HARDWIRED FOR PRECIP
-      ENDDO
-
-      WRITE(6,280) IGDN,JPDS(4),JPDS(5),IMAX,JMAX,KMAX
-  280 FORMAT(' IGDN, IMAX,JMAX,KMAX ',6I5)
-      RETURN
       END
