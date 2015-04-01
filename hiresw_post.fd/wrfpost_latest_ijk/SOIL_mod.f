dfb39ad97f470cfa7f14f1622621e320f5015a89
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2015-04-01 11:36:40 +0000

    Deletes some relic items from hrw/trunk

--- hiresw_post.fd/wrfpost_latest_ijk/SOIL_mod.f
+++
@@ -1,4 +0,0 @@
-      module soil
-      real, allocatable :: STC(:,:,:),SMC(:,:,:),SH2O(:,:,:)
-     & ,SLDPTH(:),RTDPTH(:)
-      end module soil
--- hiresw_post.fd/wrfpost_latest_ijk/try.f
+++
@@ -1,5 +0,0 @@
-program try
-! test code
-    use foo
-    write(*,*), some_parameter
-end program try
