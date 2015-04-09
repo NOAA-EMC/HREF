16dd7e954a5a1c7b6e48ea408b74114e72a70b42
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2015-04-09 18:33:01 +0000

    Big set of cleanup items, changing variable names within ush scripts to be more NCO-standards like.
    cyc --> cycloc
    run --> cyc

--- href.v1.0.0/jobs/test.sh
+++ href.v1.0.0/jobs/test.sh
@@ -1,3 +1,6 @@
-export RUN_ENVIR=dev2
-cyc=$1
-./JHREF_RUNALL.ecf $cyc
+export RUN_ENVIR=test
+export envir=test
+export SUB_ENVIR=dev2
+export cyc=$1
+
+./JHREF_RUNALL.ecf
--- href.v1.0.0/ush/href_enprod_poe.sh
+++ href.v1.0.0/ush/href_enprod_poe.sh
@@ -3,18 +3,14 @@
 #BSUB -a poe
 #BSUB -n FHR
 #BSUB -x
-#BSUB -c 1:FHR
 #BSUB -oo RUNDIR/out.poe_ensprod
 #BSUB -eo RUNDIR/err.poe_ensprod
-#BSUB -R "span[ptile=1]" -R "rusage[mem=25000]" -R "affinity[core]" 
+#BSUB -R "span[ptile=1]" 
+#BSUB -R "affinity[core]" 
 #BSUB -J "href_poe_FHR"
 #BSUB -W 00:30
-##BSUB -q "transfer"
 #BSUB -q "RUNENVIR"
 #BSUB -P HRW-T2O
-
-typeset -Z2 run
-
 
 export MP_CMDFILE=RUNDIR/run_ensprod_poe_ALL.sh
 export MP_PGMMODEL=mpmd
@@ -23,6 +19,5 @@ export MP_LABELIO=YES
 export MP_INFOLEVEL=3 
 export MP_PGMMODEL=mpmd
 mpirun.lsf 
-
 
 echo "DONE" > RUNDIR/donepoe_PIECE
