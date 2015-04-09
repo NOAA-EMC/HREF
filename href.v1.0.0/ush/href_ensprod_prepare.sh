4af13702d92563ee9f637be7f25dca6148ea0613
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2015-04-09 18:33:01 +0000

    Big set of cleanup items, changing variable names within ush scripts to be more NCO-standards like.
    cyc --> cycloc
    run --> cyc

--- href.v1.0.0/ush/href_ensprod_prepare.sh
+++ href.v1.0.0/ush/href_ensprod_prepare.sh
@@ -20,18 +20,10 @@
 export XLFRTEOPTS="namelist=old"
 
 
-run=$1   #cycles
-T=$2
+yy=`echo ${PDY} | cut -c 1-4`
+mm=`echo ${PDY} | cut -c 5-6`
+dd=`echo ${PDY} | cut -c 7-8`
 
-YS=`ndate -24 ${T}12`
-Y=`echo ${YS} | cut -c 1-8`
-yy=`date +%Y`
-mm=`date +%m`
-dd=`date +%d`
-
-yy=`echo ${T} | cut -c 1-4`
-mm=`echo ${T} | cut -c 5-6`
-dd=`echo ${T} | cut -c 7-8`
 
 rundir=$run_dir
 cd $rundir
@@ -44,103 +36,100 @@ cp $PARMhref/href_variable_grib2.tbl variable.tbl
 
 ###############################
 
-typeset -Z2 cyc     #temp variable here
-typeset -Z2 run     #cycles
+typeset -Z2 cycloc     #temp variable here
 typeset -Z2 fcst    
 typeset -Z2 m
 
-if [ $run -ge 0 ] && [ $run -le 5 ] ; then
+
+if [ $cyc -ge 0 ] && [ $cyc -le 5 ] ; then
 
   files="9 namnest namnest namnest namnest namnest conusarw conusnmmb conusarw conusnmmb conusarw conusnmmb"
   set -A file  $files
   mbrs=" 6  7  8  9 10 11 1  2  3  4  5"
 
-  if [ $run = '00' ] ; then
-    days="9 $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '00' ] ; then
+    days="9 $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
     cycs="9 00 18 12 06 00 00 00 12 12 00 00"
     ages="9  0  6 12 18 24  0  0 12 12 24 24"
     set -A  day  $days
-    set -A  cyc $cycs
+    set -A  cycloc $cycs
     set -A  age  $ages
   fi
 
 
-  if [ $run = '03' ] ; then
-    days="9 $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '03' ] ; then
+    days="9 $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
     cycs="9 00 18 12 06 00 00 00 12 12 00 00"
     ages="9  3  9 15 21 27  3  3 15 15 27 27"
     set -A  day $days
-    set -A  cyc $cycs
+    set -A  cycloc $cycs
     set -A  age $ages
   fi
 
-
-elif [ $run -ge 6 ] ; then
-
+elif [ $cyc -ge 6 ] ; then
 
   files="9 namnest namnest namnest namnest namnest namnest conusarw conusnmmb conusarw conusnmmb conusarw conusnmmb"
   set -A file  $files
   mbrs=" 7  8  9 10 11 12 1  2  3  4  5  6" 
 
-  if [ $run = '06' ] ; then
-     days="9 $T $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '06' ] ; then
+     days="9 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
      cycs="9 06 00 18 12 06 00 00 00 12 12 00 00"
      ages="9  0  6 12 18 24 30  6  6 18 18 30 30"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '09' ] ; then
-     days="9 $T $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '09' ] ; then
+     days="9 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
      cycs="9 06 00 18 12 06 00 00 00 12 12 00 00"
      ages="9  3  9 15 21 27 33  9  9 21 21 33 33"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '12' ] ; then
-     days="9 $T $T $T $Y $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '12' ] ; then
+     days="9 $PDY $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 12 06 00 18 12 06 12 12 00 00 12 12"
      ages="9  0  6 12 18 24 30  0  0 12 12 24 24"
 
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '15' ] ; then
-     days="9 $T $T $T $Y $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '15' ] ; then
+     days="9 $PDY $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 12 06 00 18 12 06 12 12 00 00 12 12"
      ages="9  3  9 15 21 27 33  3  3 15 15 27 27"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '18' ] ; then
-     days="9 $T $T $T $T $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '18' ] ; then
+     days="9 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 18 12 06 00 18 12 12 12 00 00 12 12"
      ages="9  0  6 12 18 24 30  6  6 18 18 30 30"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '21' ] ; then
-     days="9 $T $T $T $T $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '21' ] ; then
+     days="9 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 18 12 06 00 18 12 12 12 00 00 12 12"
      ages="9  3  9 15 21 27 33  9  9 21 21 33 33"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-
 else
 
- echo $run ' is not a cycle'
+ echo $cyc ' is not a cycle'
 
 fi
 
@@ -157,15 +146,15 @@ for ff in 03 06 09 12 15 18 21 24 27 30 33 36 ; do
         weight='0'$weight
       fi
 
-   if [ -s $rundir/href.m${m}.t${run}z.f$ff ] ; then
+   if [ -s $rundir/href.m${m}.t${cyc}z.f$ff ] ; then
        nmbr=` expr $nmbr + 1`
-       echo "   "$weight href.m${m}.t${run}z.f$ff "->" ${file[$m]}.t${cyc[$m]}z.f${fcst} >> temp.f${ff}
-       ln -sf $rundir/href.m${m}.t${run}z.f$ff $rundir/$ff/href.m${m}.t${run}z.f$ff
-       ln -sf $rundir/href.m${m}.t${run}z.f$ff $rundir/$ff/prcip.m${m}.t${run}z.f$ff
+       echo "   "$weight href.m${m}.t${cyc}z.f$ff "->" ${file[$m]}.t${cycloc[$m]}z.f${fcst} >> temp.f${ff}
+       ln -sf $rundir/href.m${m}.t${cyc}z.f$ff $rundir/$ff/href.m${m}.t${cyc}z.f$ff
+       ln -sf $rundir/href.m${m}.t${cyc}z.f$ff $rundir/$ff/prcip.m${m}.t${cyc}z.f$ff
    fi
  done
 
-  echo $yy $mm $dd $run $ff "227 39" "36" "3" "12"  > filename.f${ff}    #first 36 is leadtime, second 12 is fcst times = leadtime/interval
+  echo $yy $mm $dd $cyc $ff "227 39" "36" "3" "12"  > filename.f${ff}    #first 36 is leadtime, second 12 is fcst times = leadtime/interval
   cat temp.f${ff} >> filename.f${ff}
   rm -f temp.f${ff}
 
@@ -182,9 +171,9 @@ for ff in 03 06 09 12 15 18; do
  #define procedures in each tasks
  echo "cd $rundir/$ff" > run_ensprod_poe_one.$ff
  echo "$rundir/$ff/href_ensprod > $rundir/$ff/output_ensprod.$ff " >> $rundir/run_ensprod_poe_one.$ff
- echo "cp $rundir/$ff/href.mean.t${run}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
- echo "cp $rundir/$ff/href.prob.t${run}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
- echo "cp $rundir/$ff/href.sprd.t${run}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
+ echo "cp $rundir/$ff/href.mean.t${cyc}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
+ echo "cp $rundir/$ff/href.prob.t${cyc}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
+ echo "cp $rundir/$ff/href.sprd.t${cyc}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
  chmod +x run_ensprod_poe_one.$ff
  #list this task into poe task-list 
  echo "$rundir/run_ensprod_poe_one.$ff" >> $rundir/run_ensprod_poe_1-6.sh
@@ -192,16 +181,16 @@ done
 
 # prepare poe script for bsub 
 chmod +x $rundir/run_ensprod_poe_1-6.sh
-sed -e "s!FHR!6!g" -e "s!RUNDIR!$rundir!g" -e "s!RUNENVIR!$RUN_ENVIR!g" -e "s!ALL!1-6!g" -e "s!PIECE!1!g"  $USHhref/href_enprod_poe.sh > $rundir/href_enprod_poe_1-6.sh
+sed -e "s!FHR!6!g" -e "s!RUNDIR!$rundir!g" -e "s!RUNENVIR!$SUB_ENVIR!g" -e "s!ALL!1-6!g" -e "s!PIECE!1!g"  $USHhref/href_enprod_poe.sh > $rundir/href_enprod_poe_1-6.sh
 chmod +x $rundir/href_enprod_poe_1-6.sh
 
 for ff in 21 24 27 30 33 36 ; do
  #define procedures in each tasks
  echo "cd $rundir/$ff" > run_ensprod_poe_one.$ff
  echo "$rundir/$ff/href_ensprod > $rundir/$ff/output_ensprod.$ff " >> $rundir/run_ensprod_poe_one.$ff
- echo "cp $rundir/$ff/href.mean.t${run}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
- echo "cp $rundir/$ff/href.prob.t${run}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
- echo "cp $rundir/$ff/href.sprd.t${run}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
+ echo "cp $rundir/$ff/href.mean.t${cyc}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
+ echo "cp $rundir/$ff/href.prob.t${cyc}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
+ echo "cp $rundir/$ff/href.sprd.t${cyc}z.f$ff $COMOUT" >> $rundir/run_ensprod_poe_one.$ff
  chmod +x run_ensprod_poe_one.$ff
  #list this task into poe task-list
  echo "$rundir/run_ensprod_poe_one.$ff" >> $rundir/run_ensprod_poe_7-12.sh
@@ -209,7 +198,7 @@ done
 
 # prepare poe script for bsub
 chmod +x $rundir/run_ensprod_poe_7-12.sh
-sed -e "s!FHR!6!g" -e "s!RUNDIR!$rundir!g" -e "s!RUNENVIR!$RUN_ENVIR!g" -e "s!ALL!7-12!g" -e "s!PIECE!2!g" $USHhref/href_enprod_poe.sh > $rundir/href_enprod_poe_7-12.sh
+sed -e "s!FHR!6!g" -e "s!RUNDIR!$rundir!g" -e "s!RUNENVIR!$SUB_ENVIR!g" -e "s!ALL!7-12!g" -e "s!PIECE!2!g" $USHhref/href_enprod_poe.sh > $rundir/href_enprod_poe_7-12.sh
 chmod +x $rundir/href_enprod_poe_7-12.sh
 
 exit
--- href.v1.0.0/ush/href_getmbr.sh
+++ href.v1.0.0/ush/href_getmbr.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh
 #######################################################################################
 #  Script of Name:href_getmbr.sh 
-#  Purpose: This script is to get softlink of previous of runs namnest, hireswnmmb and 
+#  Purpose: this script is to get softlink of previous of runs namnest, hireswnmmb and 
 #           hireswarw according to time table (since all of them are on same #227 grid
 #           no copygb2 is involved) 
 #  History: 2015-02-02: Binbin Zhou created 
@@ -9,121 +9,112 @@
 ######################################################################################
 set -x         
 
-typeset -Z2 cyc
-typeset -Z2 run
+typeset -Z2 cycloc
 typeset -Z2 fcst
 typeset -Z2 m
-fhr=$1
-run=$2
-T=$3
 
-YS=`ndate -24 ${T}12`
-Y=`echo ${YS} | cut -c 1-8`
-#$mkdir -p $DATA/$T
-#mkdir -p $DATA/$Y
-#run_dir=$DATA/run_dir.${T}${run}
-#mkdir -p $run_dir
+fhr=$1
 
 cd $run_dir
 
 
 
-if [ $run -ge 0 ] && [ $run -le 5 ] ; then 
+if [ $cyc -ge 0 ] && [ $cyc -le 5 ] ; then 
 
   files="9 namnest namnest namnest namnest namnest conusarw conusnmmb conusarw conusnmmb conusarw conusnmmb"
   set -A file  $files
 
-  if [ $run = '00' ] ; then
-    days="9 $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '00' ] ; then
+    days="9 $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
     cycs="9 00 18 12 06 00 00 00 12 12 00 00"
     ages="9  0  6 12 18 24  0  0 12 12 24 24"
     mbrs="   1  2  3  4  5  6  7  8  9 10 11"
     set -A  day  $days
-    set -A  cyc $cycs
+    set -A  cycloc $cycs
     set -A  age  $ages
   fi
 
 
-  if [ $run = '03' ] ; then
-    days="9 $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '03' ] ; then
+    days="9 $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
     cycs="9 00 18 12 06 00 00 00 12 12 00 00"
     ages="9  3  9 15 21 27  3  3 15 15 27 27"
     mbrs="   1  2  3  4  5  6  7  8  9 10 11"
     set -A  day $days
-    set -A  cyc $cycs
+    set -A  cycloc $cycs
     set -A  age $ages
   fi
 
-elif [ $run -ge 6 ] ; then
+elif [ $cyc -ge 6 ] ; then
 
   files="9 namnest namnest namnest namnest namnest namnest conusarw conusnmmb conusarw conusnmmb conusarw conusnmmb"
   set -A file  $files
 
-  if [ $run = '06' ] ; then
-     days="9 $T $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '06' ] ; then
+     days="9 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
      cycs="9 06 00 18 12 06 00 00 00 12 12 00 00"
      ages="9  0  6 12 18 24 30  6  6 18 18 30 30"
      mbrs="   1  2  3  4  5  6  7  8  9 10 11 12"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '09' ] ; then
-     days="9 $T $T $Y $Y $Y $Y $T $T $Y $Y $Y $Y"
+  if [ $cyc = '09' ] ; then
+     days="9 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDYm1"
      cycs="9 06 00 18 12 06 00 00 00 12 12 00 00"
      ages="9  3  9 15 21 27 33  9  9 21 21 33 33"
      mbrs="   1  2  3  4  5  6  7  8  9 10 11 12"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '12' ] ; then
-     days="9 $T $T $T $Y $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '12' ] ; then
+     days="9 $PDY $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 12 06 00 18 12 06 12 12 00 00 12 12"
      ages="9  0  6 12 18 24 30  0  0 12 12 24 24"
      mbrs="   1  2  3  4  5  6  7  8  9 10 11 12"
      
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '15' ] ; then
-     days="9 $T $T $T $Y $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '15' ] ; then
+     days="9 $PDY $PDY $PDY $PDYm1 $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 12 06 00 18 12 06 12 12 00 00 12 12"
      ages="9  3  9 15 21 27 33  3  3 15 15 27 27"
      mbrs="   1  2  3  4  5  6  7  8  9 10 11 12"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '18' ] ; then
-     days="9 $T $T $T $T $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '18' ] ; then
+     days="9 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 18 12 06 00 18 12 12 12 00 00 12 12"
      ages="9  0  6 12 18 24 30  6  6 18 18 30 30"
      mbrs="   1  2  3  4  5  6  7  8  9 10 11 12"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
-  if [ $run = '21' ] ; then
-     days="9 $T $T $T $T $Y $Y $T $T $T $T $Y $Y"
+  if [ $cyc = '21' ] ; then
+     days="9 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1 $PDY $PDY $PDY $PDY $PDYm1 $PDYm1"
      cycs="9 18 12 06 00 18 12 12 12 00 00 12 12"
      ages="9  3  9 15 21 27 33  9  9 21 21 33 33"
      mbrs="   1  2  3  4  5  6  7  8  9 10 11 12"
      set -A  day $days
-     set -A  cyc $cycs
+     set -A  cycloc $cycs
      set -A  age $ages
   fi
 
 
 else
 
- echo $run ' is not a cycle'
+ echo $cyc ' is not a cycle'
 
 fi
 
@@ -140,22 +131,48 @@ for ff in $fhr ; do
       echo ff $ff
       echo fcst $fcst
 
-      echo href.m${m}.t${run}z.f${ff} 
+      echo href.m${m}.t${cyc}z.f${ff} 
 
       if [ ${file[$m]} = 'namnest' ] ; then     
-        ln -sf ${COMINNAM}.${day[$m]}/nam.t${cyc[$m]}z.conusnest.hiresf${fcst}.tm00.grib2 $run_dir/href.m${m}.t${run}z.f${ff}
-        ln -sf ${COMINNAM}.${day[$m]}/nam.t${cyc[$m]}z.conusnest.hiresf${fcst}.tm00.grib2 $run_dir/${ff}/href.m${m}.t${run}z.f${ff}
+        ln -sf ${COMINNAM}.${day[$m]}/nam.t${cycloc[$m]}z.conusnest.hiresf${fcst}.tm00.grib2 $run_dir/href.m${m}.t${cyc}z.f${ff}
+        ln -sf ${COMINNAM}.${day[$m]}/nam.t${cycloc[$m]}z.conusnest.hiresf${fcst}.tm00.grib2 $run_dir/${ff}/href.m${m}.t${cyc}z.f${ff}
       fi
  
+### for conusarw and conusnmmb, the current operational filenames are linked
+### the commented out lines have the appropriate names for the parallel HiresW system.
 
-      if [ ${file[$m]} = 'conusarw' ] || [ ${file[$m]} = 'conusnmmb' ] ; then
+      if [ ${file[$m]} = 'conusarw' ] ; then
 	
-        if [ -e ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cyc[$m]}z.awp5kmf${fcst}.grib2 ]
+        if [ -e ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cycloc[$m]}z.awp5kmf${fcst}.grib2 ]
+#        if [ -e ${COMINHIRESW}.${day[$m]}/hiresw.t${cycloc[$m]}z.arw_5km.f${fcst}.conus.grib2 ]
+
         then
-        ln -sf ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cyc[$m]}z.awp5kmf${fcst}.grib2 $run_dir/href.m${m}.t${run}z.f${ff}
-        ln -sf ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cyc[$m]}z.awp5kmf${fcst}.grib2 $run_dir/${ff}/href.m${m}.t${run}z.f${ff}
+        ln -sf ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cycloc[$m]}z.awp5kmf${fcst}.grib2 $run_dir/href.m${m}.t${cyc}z.f${ff}
+        ln -sf ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cycloc[$m]}z.awp5kmf${fcst}.grib2 $run_dir/${ff}/href.m${m}.t${cyc}z.f${ff}
+
+#        ln -sf ${COMINHIRESW}.${day[$m]}/hiresw.t${cycloc[$m]}z.arw_5km.f${fcst}.conus.grib2 $run_dir/href.m${m}.t${cyc}z.f${ff}
+#        ln -sf ${COMINHIRESW}.${day[$m]}/hiresw.t${cycloc[$m]}z.arw_5km.f${fcst}.conus.grib2 $run_dir/${ff}/href.m${m}.t${cyc}z.f${ff}
+
         fi
+
       fi
+
+      if [ ${file[$m]} = 'conusnmmb' ] ; then
+	
+        if [ -e ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cycloc[$m]}z.awp5kmf${fcst}.grib2 ]
+#        if [ -e ${COMINHIRESW}.${day[$m]}/hiresw.t${cycloc[$m]}z.nmmb_5km.f${fcst}.conus.grib2 ]
+        then
+
+        ln -sf ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cycloc[$m]}z.awp5kmf${fcst}.grib2 $run_dir/href.m${m}.t${cyc}z.f${ff}
+        ln -sf ${COMINHIRESW}.${day[$m]}/${file[$m]}.t${cycloc[$m]}z.awp5kmf${fcst}.grib2 $run_dir/${ff}/href.m${m}.t${cyc}z.f${ff}
+
+#        ln -sf ${COMINHIRESW}.${day[$m]}/hiresw.t${cycloc[$m]}z.nmmb_5km.f${fcst}.conus.grib2 $run_dir/href.m${m}.t${cyc}z.f${ff}
+#        ln -sf ${COMINHIRESW}.${day[$m]}/hiresw.t${cycloc[$m]}z.nmmb_5km.f${fcst}.conus.grib2 $run_dir/${ff}/href.m${m}.t${cyc}z.f${ff}
+
+        fi
+
+      fi
+
 
    done
 done
