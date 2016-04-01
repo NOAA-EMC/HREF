ec4a2d34b1fcd44b7420ec7d0dba819e5245e782
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2016-04-01 16:05:04 +0000

    Put ops v6.1.7 as hiresw.v6.1.8 trunk

---
+++ hiresw.v6.1.8/sorc/hiresw_wrfarwfcst.fd/external/io_int/test_io_idx.f
@@ -0,0 +1,10 @@
+
+
+
+program test_io_idx
+   print *,'NO TEST PROGRAM MADE for test_io_idx'
+   print *,'NO TEST PROGRAM MADE for test_io_idx'
+   print *,'NO TEST PROGRAM MADE for test_io_idx'
+   print *,'NO TEST PROGRAM MADE for test_io_idx'
+end program test_io_idx
+
