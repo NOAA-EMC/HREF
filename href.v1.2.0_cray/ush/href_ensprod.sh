bc49ffa14228ef89d983d64721c08000fc1ebc61
matthew.pyle@noaa.gov <matthew.pyle@noaa.gov>
2016-06-15 12:47:18 +0000

    Mostly stuff to send some output to COMOUTPERM on noscrub.

--- href.v1.2.0_cray/ush/href_ensprod.sh
+++ href.v1.2.0_cray/ush/href_ensprod.sh
@@ -147,6 +147,18 @@ if [ $SENDCOM = YES ]; then
  $WGRIB2 $COMOUT/href.t${cyc}z.pmmn.f$ff.grib2  -s >  $COMOUT/href.t${cyc}z.pmmn.f$ff.grib2.idx
  cp $DATA/$ff/href.avrg.t${cyc}z.f$ff $COMOUT/href.t${cyc}z.avrg.f$ff.grib2
  $WGRIB2 $COMOUT/href.t${cyc}z.avrg.f$ff.grib2  -s >  $COMOUT/href.t${cyc}z.avrg.f$ff.grib2.idx
+
+ cp $DATA/$ff/href.mean.t${cyc}z.f$ff $COMOUTPERM/href.t${cyc}z.mean.f$ff.grib2
+ $WGRIB2 $COMOUTPERM/href.t${cyc}z.mean.f$ff.grib2  -s >  $COMOUTPERM/href.t${cyc}z.mean.f$ff.grib2.idx
+ cp $DATA/$ff/href.prob.t${cyc}z.f$ff $COMOUTPERM/href.t${cyc}z.prob.f$ff.grib2
+ $WGRIB2 $COMOUTPERM/href.t${cyc}z.prob.f$ff.grib2  -s >  $COMOUTPERM/href.t${cyc}z.prob.f$ff.grib2.idx
+# cp $DATA/$ff/href.sprd.t${cyc}z.f$ff $COMOUTPERM/href.t${cyc}z.sprd.f$ff.grib2
+# $WGRIB2 $COMOUTPERM/href.t${cyc}z.sprd.f$ff.grib2  -s >  $COMOUTPERM/href.t${cyc}z.sprd.f$ff.grib2.idx
+ cp $DATA/$ff/href.pmmn.t${cyc}z.f$ff $COMOUTPERM/href.t${cyc}z.pmmn.f$ff.grib2
+ $WGRIB2 $COMOUTPERM/href.t${cyc}z.pmmn.f$ff.grib2  -s >  $COMOUTPERM/href.t${cyc}z.pmmn.f$ff.grib2.idx
+ cp $DATA/$ff/href.avrg.t${cyc}z.f$ff $COMOUTPERM/href.t${cyc}z.avrg.f$ff.grib2
+ $WGRIB2 $COMOUTPERM/href.t${cyc}z.avrg.f$ff.grib2  -s >  $COMOUTPERM/href.t${cyc}z.avrg.f$ff.grib2.idx
+
 fi
 if [ $SENDDBN = YES ]; then
   $DBNROOT/bin/dbn_alert MODEL HREF_GB2 $job $COMOUT/href.t${cyc}z.mean.f$ff.grib2
