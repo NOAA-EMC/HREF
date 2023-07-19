c922a5414ddea23a81a2551a55a0d5713526f3dd
matthew pyle <Matthew.Pyle@noaa.gov>
2023-07-19 19:55:59 +0000

    Fixes location of enspost fix files.

--- sorc/link_enspost_fix.sh
+++ sorc/link_enspost_fix.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 # ENSPOST_FIX=/lfs/h2/emc/lam/noscrub/Jun.Du/rrfs.v1.0.0/fix
-ENSPOST_FIX=/lfs/h2/emc/lam/noscrub/Matthew.Pyle/rrfs.v1.0.0/fix
+ENSPOST_FIX=/lfs/h2/emc/lam/noscrub/Matthew.Pyle/rrfs_enspost_fix
+
 
 
 mkdir -p ../fix
