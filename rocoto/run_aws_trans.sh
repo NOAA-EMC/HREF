0d30afac536edd2734a3db300b7ba9b95f286e7e
matthew pyle <Matthew.Pyle@noaa.gov>
2023-08-29 14:26:12 +0000

    Pushes the updated .xml and run_aws_trans jobs being used into github.

--- rocoto/run_aws_trans.sh
+++ rocoto/run_aws_trans.sh
@@ -8,6 +8,19 @@ echo WGRIB2 is $WGRIB2
 
 dir="/lfs/h2/emc/lam/noscrub/emc.lam/enspost/rrfs.v1.0.0/rocoto"
 
+# make sure we are on prod machine
+
+qstat -Q | grep devmax | grep no
+err=$?
+
+if [ $err -ne 0 ]
+then
+                echo "NOT prod machine, so exit"
+                        exit
+                else
+                        echo "looks like prod, so proceed"
+fi
+
 thing=drive_rrfsa_enspost
 rocotorun -v 10 -w ${dir}/${thing}.xml -d ${dir}/${thing}.db
 
