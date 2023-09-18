d1e73f8e1708d127810f2ae12208ac4e4fdef79a
Matthew Pyle <matthew.pyle@noaa.gov>
2023-09-18 15:53:42 +0000

    pushes changes that splits the 1 h, 3 h, and 6 h QPF EAS processing into low/med/high categories similar to what is done for 12 h and 24 h QPF processing.  Still a bit of a work in progress.  Doing from 2 nodes to 4 nodes, and with more tasks, it sped up the AK and CONUS EAS jobs by roughly 6-7 minutes (say from 33 minutes to 27 minutes).

--- ush/eas_config.py
+++ ush/eas_config.py
@@ -29,9 +29,20 @@ pqpf_6h_thresh = [.01,0.25,0.5,1.0,2.0,3.0,5.0]
 pqpf_12h_thresh = [0.1,0.25,0.5,1.0,2.0,3.0,5.0,8.0]
 pqpf_24h_thresh = [0.1,0.25,0.5,1.0,2.0,3.0,5.0,8.0]
 
+pqpf_1h_thresh_low = [.01,0.25,0.5]
+pqpf_1h_thresh_high = [1.0,2.0]
+
+pqpf_3h_thresh_low = [.01,0.25,0.5]
+pqpf_3h_thresh_high = [1.0,2.0,3.0]
+
+pqpf_6h_thresh_low = [.01,0.25]
+pqpf_6h_thresh_med = [0.5,1.0,2.0]
+pqpf_6h_thresh_high = [3.0,5.0]
+
 pqpf_12h_thresh_low = [0.1,0.25,0.5]
 pqpf_12h_thresh_med = [1.0,2.0,3.0]
 pqpf_12h_thresh_high = [5.0,8.0]
+
 pqpf_24h_thresh_low = [0.1,0.25,0.5]
 pqpf_24h_thresh_med = [1.0,2.0,3.0]
 pqpf_24h_thresh_high = [5.0,8.0]
