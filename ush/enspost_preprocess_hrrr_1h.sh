056c124d7816c488c3c83d2cae18b50887c049ff
matthew pyle <Matthew.Pyle@noaa.gov>
2023-09-21 15:50:34 +0000

    Adds some initial HRRR preprocessing pieces from HREF, and cleans out HREF ex-scripts.

---
+++ ush/enspost_preprocess_hrrr_1h.sh
@@ -0,0 +1,91 @@
+#! /bin/ksh
+#####################################################
+#
+#
+#  Script: preprocess_hrrr_1h.sh.ecf
+#
+# Purpose: Filters out needed inputs from HRRR for HREF,
+#          and interpolates onto the HREF domain.
+#
+#  Author: Matthew Pyle
+#          March 2020
+
+set -x
+
+if [ $# -ne 3 ]
+then
+echo "ERROR: need cycle and forecast hour and nest"
+exit
+fi
+
+cyc=${1}
+NEST=${2}
+hr=${3}
+
+echo $NEST $hr
+
+if [ ! -e $GESOUT.${PDY} ]
+then
+mkdir -p $GESOUT.${PDY}
+fi
+
+cd ${DATA}
+
+mkdir hrrr_${NEST}_${hr}
+cd hrrr_${NEST}_${hr}
+
+
+if [ $NEST = "conus" ]
+then
+ wgrib2def="lambert:265:25:25 226.541:1473:5079 12.190:1025:5079"
+ NESTLOC=conus
+ filecheck=${COMINhrrr}.${PDY}/${NESTLOC}/hrrr.t${cyc}z.wrfprsf${hr}.grib2
+elif [ $NEST = "ak" ]
+then 
+ NESTLOC=alaska
+ wgrib2def="nps:210:60 185.5:825:5000 44.8:603:5000"
+ filecheck=${COMINhrrr}.${PDY}/${NESTLOC}/hrrr.t${cyc}z.wrfprsf${hr}.ak.grib2
+fi
+
+
+        if [ -e $filecheck ]
+        then
+
+         $WGRIB2 $filecheck | grep -F -f $PARMhref/href_hrrr_filter.txt | $WGRIB2 -i -grib hrrr.t${cyc}z.f${hr} $filecheck
+         $WGRIB2 $filecheck -match ":(HINDEX|TSOIL|SOILW|CSNOW|CICEP|CFRZR|CRAIN|REFD|MAXREF|APCP):" -grib nn.t${cyc}z.f${hr}.grb
+         $WGRIB2 $filecheck -match "LTNG" -set_byte 4 23 1 -grib ltng.t${cyc}z.f${hr}.grb
+         $WGRIB2 $filecheck -match "MSLMA" -set_byte 4 11 192 -grib mslet.t${cyc}z.f${hr}.grb
+
+	 $WGRIB2 $filecheck -match "HPBL" -set_byte 4 10 3 -grib pblh_start.t${cyc}z.f${hr}.grb
+	 $WGRIB2 pblh_start.t${cyc}z.f${hr}.grb -set_byte 4 11 5 -grib pblh_mid.t${cyc}z.f${hr}.grb
+	 $WGRIB2 pblh_mid.t${cyc}z.f${hr}.grb -set_byte 4 23 220 -grib pblh.t${cyc}z.f${hr}.grb
+
+         $WGRIB2 $filecheck -match "RETOP" -set_byte 4 23 200 -grib retop.t${cyc}z.f${hr}.grb
+         $WGRIB2 retop.t${cyc}z.f${hr}.grb -set_byte 4 11 197 -grib new_retop.t${cyc}z.f${hr}.grb
+         mv  new_retop.t${cyc}z.f${hr}.grb retop.t${cyc}z.f${hr}.grb
+
+         $WGRIB2 $filecheck -match "REFC" -set_byte 4 23 200 -grib refc.t${cyc}z.f${hr}.grb
+         $WGRIB2 $filecheck -match "TCDC" -set_byte 4 23 200 -grib tcdc.t${cyc}z.f${hr}.grb
+
+         $WGRIB2 $filecheck -match "WEASD" -match "hour acc fcst" -grib nn2.t${cyc}z.f${hr}.grb
+         $WGRIB2 $filecheck -match "HGT:cloud ceiling:" -grib ceiling.t${cyc}z.f${hr}.grb
+
+         cat nn.t${cyc}z.f${hr}.grb  nn2.t${cyc}z.f${hr}.grb ceiling.t${cyc}z.f${hr}.grb retop.t${cyc}z.f${hr}.grb  \
+         refc.t${cyc}z.f${hr}.grb tcdc.t${cyc}z.f${hr}.grb ltng.t${cyc}z.f${hr}.grb > inputs_nn.t${cyc}z.f${hr}.grb
+
+         rm nn.t${cyc}z.f${hr}.grb  nn2.t${cyc}z.f${hr}.grb ceiling.t${cyc}z.f${hr}.grb retop.t${cyc}z.f${hr}.grb  \
+         refc.t${cyc}z.f${hr}.grb tcdc.t${cyc}z.f${hr}.grb ltng.t${cyc}z.f${hr}.grb 
+
+	 cat mslet.t${cyc}z.f${hr}.grb pblh.t${cyc}z.f${hr}.grb >> hrrr.t${cyc}z.f${hr}
+         rm mslet.t${cyc}z.f${hr}.grb pblh*.t${cyc}z.f${hr}.grb
+         $WGRIB2 hrrr.t${cyc}z.f${hr} -set_grib_type  jpeg -new_grid_winds grid -new_grid ${wgrib2def} interp.t${cyc}z.f${hr}
+         $WGRIB2  inputs_nn.t${cyc}z.f${hr}.grb -new_grid_interpolation neighbor -set_grib_type jpeg -new_grid_winds grid -new_grid ${wgrib2def} interp_nn.t${cyc}z.f${hr}
+
+         cat interp.t${cyc}z.f${hr}  interp_nn.t${cyc}z.f${hr}  > ../hrrr.t${cyc}z.${NEST}.f${hr}.grib2
+         rm interp.t${cyc}z.f${hr}  interp_nn.t${cyc}z.f${hr}  inputs_nn.t${cyc}z.f${hr}.grb   hrrr.t${cyc}z.f${hr}
+
+        else
+         msg="FATAL ERROR: $filecheck missing"
+         err_exit $msg
+        fi
+
---
+++ ush/enspost_preprocess_hrrr_3hapcp.sh
@@ -0,0 +1,87 @@
+#! /bin/ksh
+#####################################################
+#
+#
+#  Script: preprocess_hrrr_3hapcp.sh.ecf
+#
+# Purpose: Generates 3 h QPF/snow buckets from the HRRR
+#
+#  Author: Matthew Pyle
+#          March 2020
+
+set -x
+
+if [ $# -ne 2 ]
+then
+echo need cycle and NEST
+exit
+fi
+
+cyc=${1}
+NEST=${2}
+
+hrs="03 06 09 12 15 18 21 24 27 30 33 36 39 42 45 48" 
+
+if [ ! -e $GESOUT.${PDY} ]
+then
+mkdir -p $GESOUT.${PDY}
+fi
+
+
+cd $DATA
+
+EXEChref=${HOMEhref}/exec
+
+hrsln="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48"
+
+for hr in $hrsln
+do
+filecheck=hrrr.t${cyc}z.${NEST}.f${hr}.grib2
+if [ -e $filecheck ]
+then
+ln -sf hrrr.t${cyc}z.${NEST}.f${hr}.grib2 hrrr.t${cyc}z.f${hr}.grib2
+fi
+done
+
+for hr in $hrs
+do
+
+let hrold=hr-3
+
+if [ $hrold -lt 10 ] 
+then
+hrold=0${hrold}
+fi
+
+filecheck=hrrr.t${cyc}z.${NEST}.f${hr}.grib2
+
+if [ -e $filecheck ]
+then
+
+        if [ $hr -gt 0 ]
+        then
+        echo here a $hr
+
+## the hourly will sum up three hourly at the appropriate time.
+        echo hrrr.t${cyc}z.f $hr .false. .false. .true. .false. .false. 1 ${NEST} |$EXEChref/href_get_prcip > output.href_get_prcip1h.f${hr}.${NEST}
+        export err=$? ; err_chk
+
+        if [ ${hr}%3 -eq 0 ]
+        then
+        cat prcip3h.t${cyc}z.f${hr}.grib2 >> hrrr.t${cyc}z.${NEST}.f${hr}.grib2
+        fi
+        fi
+
+else
+        msg="FATAL ERROR: $filecheck missing"
+        err_exit $msg
+fi
+
+done
+
+for hr in $hrsln
+do
+ cp hrrr.t${cyc}z.${NEST}.f${hr}.grib2 ${GESOUT}.${PDY}
+ err=$?
+ export err ; err_chk
+done
