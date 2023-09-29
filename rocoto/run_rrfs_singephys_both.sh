e459800c11ec274854ee1bb1f3b4a5910a710b16
matthew pyle <Matthew.Pyle@noaa.gov>
2023-09-29 21:08:27 +0000

    Initial commit to 1.2.0_winter branch just getting things going.

--- rocoto/run_rrfs_singephys_both.sh
+++ rocoto/run_rrfs_singephys_both.sh
@@ -4,21 +4,18 @@ module load core/rocoto/1.3.5
 
 module list
 
-doms="conus hi pr ak"
+doms="ak"
 
 echo WGRIB2 is $WGRIB2
 
-dir="/lfs/h2/emc/lam/noscrub/emc.lam/enspost/rrfs.v1.0.0/rocoto"
+dir="/lfs/h2/emc/lam/noscrub/Matthew.Pyle/enspost/rrfs.v1.2.0_winter/rocoto"
 
 for dom in $doms
 do
 
 rocotorun -v 10 -w ${dir}/drive_rrfs_enspost_${dom}_singlephys_timelag.xml -d ${dir}/drive_rrfs_enspost_${dom}_singlephys_timelag.db
 
-sleep 60
+# sleep 60
 
-# sleep 120
-
-# rocotorun -v 10 -w ${dir}/drive_rrfs_enspost_${dom}_singlephys_single.xml -d ${dir}/drive_rrfs_enspost_${dom}_singlephys_single.db
 
 done
