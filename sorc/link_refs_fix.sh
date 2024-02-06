46c184454f08aacf77b025c90b604b8bde38bc65
matthew pyle <Matthew.Pyle@noaa.gov>
2024-02-06 20:26:14 +0000

    Updates link_refs_fix.sh

--- sorc/link_refs_fix.sh
+++ sorc/link_refs_fix.sh
@@ -1,15 +1,10 @@
 #! /bin/sh
 
-# ENSPOST_FIX=/lfs/h2/emc/lam/noscrub/Jun.Du/rrfs.v1.0.0/fix
-ENSPOST_FIX=/lfs/h2/emc/lam/noscrub/Matthew.Pyle/rrfs_enspost_fix
-
-
+ENSPOST_FIX=/lfs/h2/emc/lam/noscrub/Matthew.Pyle/refs_enspost_fix
 
 mkdir -p ../fix
 
 cd ../fix
-
-# ln -sf ${ENSPOST_FIX}/* .
 
 cp ${ENSPOST_FIX}/* .
 
