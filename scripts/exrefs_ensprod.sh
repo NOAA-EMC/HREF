db54e2e4eaa2327c2581d05cc081a6c6773a9666
Matthew Pyle <matthew.pyle@noaa.gov>
2026-06-24 11:47:13 +0000

    Removes TMP record before copying prcip file.

--- scripts/exrefs_ensprod.sh
+++ scripts/exrefs_ensprod.sh
@@ -89,7 +89,8 @@ do
 
 if [ -e $DATA/prcip.m${m}.t${cyc}z.f${fhr} ]
 then
-cpreq $DATA/prcip.m${m}.t${cyc}z.f${fhr} $COMOUT/../../refsprep/refs.${PDY}/${cyc}/prcip/prcip.t${cyc}z.${NEST}.m${m}.f${fhr}.grib2
+$WGRIB2 $DATA/prcip.m${m}.t${cyc}z.f${fhr} -not TMP $DATA/prcip.m${m}.t${cyc}z.f${fhr}.notemp
+cpreq $DATA/prcip.m${m}.t${cyc}z.f${fhr}.notemp $COMOUT/../../refsprep/refs.${PDY}/${cyc}/prcip/prcip.t${cyc}z.${NEST}.m${m}.f${fhr}.grib2
 fi
 
 done
